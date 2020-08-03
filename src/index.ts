7546ee818212efb15604a9b2c94e313c7fe55ca6
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-03 13:10:25 +0200

    Add HTMLInputElement to inputfields constants

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,52 @@ const root = document.documentElement;
 const hexagonContainer = document.getElementById('hexagon__container');
 
 // INPUT FIELD
-const backgroundColor = document.getElementById('bg-color');
-const hexagonColor = document.getElementById('hexagon-color');
-const textColor = document.getElementById('text-color');
-const hexagonSize = document.getElementById('hexagon-size');
-const containerSkewX = document.getElementById('container-skew-X');
-const containerSkewY = document.getElementById('container-skew-Y');
-const hexagonGap = document.getElementById('hexagon-gap');
-const hexagonAmount = document.getElementById('hexagon-amount');
-const hexagonFirstRow = document.getElementById('hexagon-first-row');
-const hexagonRotation = document.getElementById('hexagon-rotation');
-const hexagonScale = document.getElementById('hexagon-scale');
-const hexagonTransition = document.getElementById('hexagon-transition');
-const mediaQuery_1 = document.getElementById('media-query--1');
-const mediaQuery_2 = document.getElementById('media-query--2');
-const mediaQuery_3 = document.getElementById('media-query--3');
+const backgroundColor = <HTMLInputElement>document.getElementById('bg-color');
+const hexagonColor = <HTMLInputElement>document.getElementById('hexagon-color');
+const textColor = <HTMLInputElement>document.getElementById('text-color');
+const hexagonSize = <HTMLInputElement>document.getElementById('hexagon-size');
+const containerSkewX = <HTMLInputElement>(
+  document.getElementById('container-skew-X')
+);
+const containerSkewY = <HTMLInputElement>(
+  document.getElementById('container-skew-Y')
+);
+const hexagonGap = <HTMLInputElement>document.getElementById('hexagon-gap');
+const hexagonAmount = <HTMLInputElement>(
+  document.getElementById('hexagon-amount')
+);
+const hexagonFirstRow = <HTMLInputElement>(
+  document.getElementById('hexagon-first-row')
+);
+const hexagonRotation = <HTMLInputElement>(
+  document.getElementById('hexagon-rotation')
+);
+const hexagonScale = <HTMLInputElement>document.getElementById('hexagon-scale');
+const hexagonTransition = <HTMLInputElement>(
+  document.getElementById('hexagon-transition')
+);
+const mediaQuery_1 = <HTMLInputElement>(
+  document.getElementById('media-query--1')
+);
+const mediaQuery_2 = <HTMLInputElement>(
+  document.getElementById('media-query--2')
+);
+const mediaQuery_3 = <HTMLInputElement>(
+  document.getElementById('media-query--3')
+);
+
+// var inputValue = (<HTMLInputElement>document.getElementById(elementId)).value;
 
 // OUTPUT HTML CSS
 const htmlTextField = document.getElementById('html');
 const cssTextField = document.getElementById('css');
+
+// FUNCTIONS
+function changeBackgroundColor(value) {
+  root.style.setProperty('--color-bg', value);
+  // generateCSStext();
+}
+
+// EVENT LISTENERS
+backgroundColor.oninput = (): void =>
+  changeBackgroundColor(backgroundColor.value);
