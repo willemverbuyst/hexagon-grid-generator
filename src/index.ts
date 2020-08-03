1b9678e711699761de262e291437af0cd6324b97
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-03 12:23:34 +0200

    add constants to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hello there');
+const root = document.documentElement;
+const hexagonContainer = document.getElementById('hexagon__container');
+
+// INPUT FIELD
+const backgroundColor = document.getElementById('bg-color');
+const hexagonColor = document.getElementById('hexagon-color');
+const textColor = document.getElementById('text-color');
+const hexagonSize = document.getElementById('hexagon-size');
+const containerSkewX = document.getElementById('container-skew-X');
+const containerSkewY = document.getElementById('container-skew-Y');
+const hexagonGap = document.getElementById('hexagon-gap');
+const hexagonAmount = document.getElementById('hexagon-amount');
+const hexagonFirstRow = document.getElementById('hexagon-first-row');
+const hexagonRotation = document.getElementById('hexagon-rotation');
+const hexagonScale = document.getElementById('hexagon-scale');
+const hexagonTransition = document.getElementById('hexagon-transition');
+const mediaQuery_1 = document.getElementById('media-query--1');
+const mediaQuery_2 = document.getElementById('media-query--2');
+const mediaQuery_3 = document.getElementById('media-query--3');
+
+// OUTPUT HTML CSS
+const htmlTextField = document.getElementById('html');
+const cssTextField = document.getElementById('css');
