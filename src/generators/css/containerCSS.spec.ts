776f1c404fb1a3a64410c751af0aa0b811b23185
Willem Verbuyst <willemverbuyst@gmail.com>
2022-04-09 16:07:08 +0200

    feat: generate container css text

---
+++ src/generators/css/containerCSS.spec.ts
@@ -0,0 +1,16 @@
+import { generateContainerCSSText } from './containerCSS'
+
+describe('#generateContainerCSSText', () => {
+	describe('given input values', () => {
+		it('should return a css-class', () => {
+			expect(generateContainerCSSText(4, 3, 2, 1).replace(/\s/g, '')).toMatch(
+				`.hexagon-wrapper__hexagon-container {
+          width: 12vw;
+          display: flex;
+          flex-wrap: wrap;
+          transform: skew(2deg, 1deg);
+      }`.replace(/\s/g, '')
+			)
+		})
+	})
+})
---
+++ src/generators/css/containerCSS.ts
@@ -0,0 +1,13 @@
+export const generateContainerCSSText = (
+	hexagonsFirstRow: number,
+	hexagonSize: number,
+	containerSkewX: number,
+	containerSkewY: number
+): string =>
+	`.hexagon-wrapper__hexagon-container {
+    width: ${hexagonsFirstRow * hexagonSize}vw;
+    display: flex;
+    flex-wrap: wrap;
+    transform: skew(${containerSkewX}deg, ${containerSkewY}deg);
+  }
+}`
