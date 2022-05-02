55151bd6599428c3cb7e7b0539d38d9d7e0569e4
Willem Verbuyst <willemverbuyst@gmail.com>
2022-05-02 05:21:15 +0200

    test: pass 1 argument to basic hexagon css

--- src/generators/css/basicHexagon.css.spec.ts
+++ src/generators/css/basicHexagon.css.spec.ts
@@ -27,5 +27,31 @@ describe('#generateBasicHexagonCSS', () => {
         align-items: center;`.replace(/\s/g, '')
 			)
 		})
+
+		it('should return string with basic css for hexagon with one argument', () => {
+			expect(generateBasicHexagonCSS(70).replace(/\s/g, '')).toMatch(
+				`-webkit-clip-path: polygon(
+          0 25%,
+          50% 0,
+          100% 25%,
+          100% 75%,
+          50% 100%,
+          0 75%
+        );
+        clip-path: polygon(
+          0 25%,
+          50% 0,
+          100% 25%,
+          100% 75%,
+          50% 100%,
+          0 75%
+        );
+        width: 70%;
+        height: 70%;
+        display: flex;
+        justify-content: center;
+        align-items: center;`.replace(/\s/g, '')
+			)
+		})
 	})
 })
