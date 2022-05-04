d4bcf7f777c0976178ead4275a6db4a3ce36f8c1
Willem Verbuyst <willemverbuyst@gmail.com>
2022-05-04 19:29:53 +0200

    feat: work on text style

--- src/logic/cssDisplay.ts
+++ src/logic/cssDisplay.ts
@@ -7,13 +7,22 @@ export const displayCSS = (): void => {
 	const cssTextField = document.getElementById('css')
 	assertNonNullish(cssTextField, 'HTMLElement #css not found!')
 
-	cssBtn.addEventListener('click', () => handleCSSBtnClick(cssTextField))
+	const htmlTextField = document.getElementById('html')
+	assertNonNullish(htmlTextField, 'HTMLElement #htmlTextField not found!')
+
+	cssBtn.addEventListener('click', () =>
+		handleCSSBtnClick(cssTextField, htmlTextField)
+	)
 }
 
-const handleCSSBtnClick = (cssTextField: HTMLElement): void => {
+const handleCSSBtnClick = (
+	cssTextField: HTMLElement,
+	htmlTextField: HTMLElement
+): void => {
 	if (cssTextField.style.visibility === 'visible') {
 		cssTextField.style.visibility = 'hidden'
 	} else {
+		htmlTextField.style.visibility = 'hidden'
 		cssTextField.style.visibility = 'visible'
 	}
 }
--- src/logic/htmlDisplay.ts
+++ src/logic/htmlDisplay.ts
@@ -4,16 +4,25 @@ export const displayHTML = (): void => {
 	const htmlBtn = document.getElementById('htmlBtn')
 	assertNonNullish(htmlBtn, 'HTMLElement #htmlBtn not found!')
 
+	const cssTextField = document.getElementById('css')
+	assertNonNullish(cssTextField, 'HTMLElement #css not found!')
+
 	const htmlTextField = document.getElementById('html')
 	assertNonNullish(htmlTextField, 'HTMLElement #htmlTextField not found!')
 
-	htmlBtn.addEventListener('click', () => handleHTMLBtnClick(htmlTextField))
+	htmlBtn.addEventListener('click', () =>
+		handleHTMLBtnClick(cssTextField, htmlTextField)
+	)
 }
 
-const handleHTMLBtnClick = (htmlTextField: HTMLElement): void => {
+const handleHTMLBtnClick = (
+	cssTextField: HTMLElement,
+	htmlTextField: HTMLElement
+): void => {
 	if (htmlTextField.style.visibility === 'visible') {
 		htmlTextField.style.visibility = 'hidden'
 	} else {
+		cssTextField.style.visibility = 'hidden'
 		htmlTextField.style.visibility = 'visible'
 	}
 }
