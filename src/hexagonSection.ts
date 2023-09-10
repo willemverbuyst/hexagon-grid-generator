98eae4b7a422ef3c22415a2ae20f442319ce4afa
Willem Verbuyst <willemverbuyst@gmail.com>
2023-09-10 06:37:23 +0200

    docs: generate hexagons

--- src/hexagonSection.ts
+++ src/hexagonSection.ts
@@ -13,29 +13,29 @@ export function generateHexagon(hexagonNumber: number, className?: string) {
 }
 
 export function generateHexagons(
-  hexagonsFirstRow: number,
-  numberOfHexagons: number,
+  numberOfHexagonsFirstRow: number,
+  totalNumberOfHexagons: number,
 ) {
   let html = "";
 
-  if (hexagonsFirstRow < 1 || numberOfHexagons < 1) {
-    return "";
+  if (numberOfHexagonsFirstRow < 1 || totalNumberOfHexagons < 1) {
+    return html;
   }
 
-  if (numberOfHexagons <= hexagonsFirstRow) {
-    Array(numberOfHexagons)
+  if (numberOfHexagonsFirstRow === 1 && totalNumberOfHexagons === 1) {
+    html = generateHexagon(1);
+    return html;
+  }
+
+  if (totalNumberOfHexagons <= numberOfHexagonsFirstRow) {
+    Array(totalNumberOfHexagons)
       .fill(0)
       .forEach((_, i) => (html += generateHexagon(i + 1)));
     return html;
   }
 
-  if (hexagonsFirstRow === 1 && numberOfHexagons === 1) {
-    html = generateHexagon(1);
-    return html;
-  }
-
-  if (hexagonsFirstRow === 1) {
-    Array(numberOfHexagons)
+  if (numberOfHexagonsFirstRow === 1) {
+    Array(totalNumberOfHexagons)
       .fill(0)
       .forEach(
         (_, i) => (html += generateHexagon(i + 1, "first-row__margin-top")),
@@ -43,17 +43,17 @@ export function generateHexagons(
     return html;
   }
 
-  Array(hexagonsFirstRow)
+  Array(numberOfHexagonsFirstRow)
     .fill(0)
     .forEach((_, i) => {
       html += generateHexagon(i + 1, "first-row__margin-top");
     });
 
   let k = 0;
-  let hexagonNumber = hexagonsFirstRow + 1;
+  let hexagonNumber = numberOfHexagonsFirstRow + 1;
   // eslint-disable-next-line no-loops/no-loops
-  while (k < numberOfHexagons - hexagonsFirstRow) {
-    if (k === 0 || k % ((hexagonsFirstRow - 1) * 2 + 1) === 0) {
+  while (k < totalNumberOfHexagons - numberOfHexagonsFirstRow) {
+    if (k === 0 || k % ((numberOfHexagonsFirstRow - 1) * 2 + 1) === 0) {
       html += generateHexagon(hexagonNumber, "even-rows__margin-left");
     } else {
       html += generateHexagon(hexagonNumber);
